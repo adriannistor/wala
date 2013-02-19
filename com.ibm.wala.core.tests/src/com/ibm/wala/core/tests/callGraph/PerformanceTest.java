c52f78d2c18c2cb52afbd9b3e80238fc96febb2b
Cosmin Radoi <cosmin.radoi@gmail.com>
2013-02-19 15:21:18 -0600

    add more performance tests

--- com.ibm.wala.core.tests/src/com/ibm/wala/core/tests/callGraph/PerformanceTest.java
+++ com.ibm.wala.core.tests/src/com/ibm/wala/core/tests/callGraph/PerformanceTest.java
@@ -16,6 +16,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.ibm.wala.classLoader.IClass;
@@ -38,7 +39,9 @@ import com.ibm.wala.ipa.cfg.InterproceduralCFG;
 import com.ibm.wala.ipa.cha.ClassHierarchy;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
 import com.ibm.wala.ipa.cha.IClassHierarchy;
+import com.ibm.wala.ssa.DefaultIRFactory;
 import com.ibm.wala.ssa.ISSABasicBlock;
+import com.ibm.wala.ssa.NoCachingSSACache;
 import com.ibm.wala.types.Descriptor;
 import com.ibm.wala.types.MethodReference;
 import com.ibm.wala.util.CancelException;
@@ -55,6 +58,7 @@ import com.ibm.wala.util.warnings.Warnings;
  * Tests for Call Graph construction
  */
 
+@Ignore
 public class PerformanceTest extends AbstractCallGraphTest {
 
   public static void main(String[] args) {
@@ -64,6 +68,37 @@ public class PerformanceTest extends AbstractCallGraphTest {
   @Test
   public void testDefault() throws ClassHierarchyException, IllegalArgumentException, CancelException, IOException {
     testFor(new AnalysisCache());
+  }
+  
+  @Test
+  public void testNoCaching() throws ClassHierarchyException, IllegalArgumentException, CancelException, IOException {
+    AnalysisCache cache = noCachingCache();
+    testFor(cache);
+  }
+  
+  @Test
+  public void testDefaultRTA() throws ClassHierarchyException, IllegalArgumentException, IOException, CancelException {
+    testDefaultRTAFor(new AnalysisCache());
+  }
+  
+  @Test
+  public void testNoCachingRTA() throws ClassHierarchyException, IllegalArgumentException, IOException, CancelException {
+    testDefaultRTAFor(noCachingCache());
+  }
+  
+  private void testDefaultRTAFor(AnalysisCache cache) throws IOException, ClassHierarchyException, IllegalArgumentException, CancelException {
+    AnalysisScope scope = CallGraphTestUtil.makeJ2SEAnalysisScope(TestConstants.BCEL, CallGraphTestUtil.REGRESSION_EXCLUSIONS);
+    ClassHierarchy cha = ClassHierarchy.make(scope);
+    Iterable<Entrypoint> entrypoints = com.ibm.wala.ipa.callgraph.impl.Util.makeMainEntrypoints(scope, cha,
+        TestConstants.BCEL_VERIFIER_MAIN);
+    AnalysisOptions options = CallGraphTestUtil.makeAnalysisOptions(scope, entrypoints);
+    CallGraphTestUtil.buildRTA(options, cache, cha, scope);
+  }
+  
+  private AnalysisCache noCachingCache() {
+    DefaultIRFactory irFactory = new DefaultIRFactory();
+    AnalysisCache cache = new AnalysisCache(irFactory, new NoCachingSSACache(irFactory));
+    return cache;
   }
 
   private void testFor(AnalysisCache cache) throws IOException, ClassHierarchyException, CancelException {
